1d1dd3d4aed88efe606eb8ad316721618551df5b
alp22 <apezza@wisc.edu>
2024-02-11 11:07:02 -0600

    structure data

---
+++ frontend/Dockerfile
@@ -0,0 +1,7 @@
+# Be sure to run 'npm run build' first!
+FROM nginx:stable-alpine
+COPY dist /usr/share/nginx/html
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { Card, Container, Row, Col, Image } from 'react-bootstrap';
+
+
+function App() {
+
+  return null;
+}
+
+export default App
